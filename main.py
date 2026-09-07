4d083d9c95da338fcfb96bb8ef3e35f2bb4a9d5c
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-07 12:08:07 +0530

    handling spaceship movements on key press

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ pygame.display.set_caption("Galaxy Fighters")
 
 WHITE = (255, 255, 255)
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+VEL = 5
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
 YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
 
@@ -19,6 +20,26 @@ def draw_window(red, yellow):
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
 
+def yellow_handle_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a]: # Left
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d]: # Right
+        yellow.x += VEL
+    if keys_pressed[pygame.K_w]: # Up
+        yellow.y -= VEL
+    if keys_pressed[pygame.K_s]: # Down
+        yellow.y += VEL
+
+def red_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT]: # Left
+        red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT]: # Right
+        red.x += VEL
+    if keys_pressed[pygame.K_UP]: # Up
+        red.y -= VEL
+    if keys_pressed[pygame.K_DOWN]: # Down
+        red.y += VEL
+
 def main():
     red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
     yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
@@ -30,6 +51,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        keys_pressed = pygame.key.get_pressed()
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
 
         draw_window(red, yellow)
 
