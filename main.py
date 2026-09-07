037f34b0bc1f491fd21a5fa9d620e29cfe681bea
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-07 11:28:23 +0530

    importing, resizing and displaying the spaceships

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import pygame
+import os
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Galaxy Fighters")
 
 WHITE = (255, 255, 255)
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
 
 def draw_window():
     WIN.fill(WHITE)
+    WIN.blit(YELLOW_SPACESHIP, (300, 100))
     pygame.display.update()
 
 def main():
-    clock = pygame.time.clock()
+    clock = pygame.time.Clock()
     run = True
 
     while run:
